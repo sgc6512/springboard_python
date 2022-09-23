31fc876a369ef6e8e81830079237731c39ff0b00
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2022-09-23 18:41:14 -0400

    Add files via upload

---
+++ flask-survey/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+responses = []
+
+@app.route('/')
+def home():
+  """Home Page for surveys"""
+  return render_template("home.html", title=satisfaction_survey.title, instruc=satisfaction_survey.instructions)
+
+@app.route('/questions/<int:q_id>')
+def show_question(q_id):
+  """Show question of given id"""
+
+  # Check if user is at the right URL
+  if q_id != len(responses):
+    flash("Trying to access invalid question. Redirecting.")
+    q_id = len(responses)
+  # If user has answered all questions redirect to thank you page
+  if len(responses) == 4:
+    return redirect("thanks.html")
+
+  return render_template("form.html", question=satisfaction_survey.questions[q_id])
+
+@app.route('/answer', methods=['POST'])
+def answers():
+  """Collect answers"""
+  responses.append(request.form["choice"])
+  if len(responses) < len(satisfaction_survey.questions):
+    return redirect("/questions/" + str(len(responses)))
+  return redirect("/thanks")
+
+@app.route('/thanks')
+def thanks():
+  return render_template("thanks.html")

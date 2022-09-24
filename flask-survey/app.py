b312c2a3399f8a259829422b3fb475f874157a8b
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2022-09-24 19:17:04 -0400

    Add files via upload

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -15,24 +15,33 @@ def home():
   """Home Page for surveys"""
   return render_template("home.html", title=satisfaction_survey.title, instruc=satisfaction_survey.instructions)
 
+@app.route('/questions', methods=['POST'])
+def initialize():
+  """Set session responses to an empty list"""
+  ls = []
+  session["responses"] = ls
+  return redirect("/questions/0")
+
 @app.route('/questions/<int:q_id>')
 def show_question(q_id):
   """Show question of given id"""
 
   # Check if user is at the right URL
-  if q_id != len(responses):
+  if q_id != len(session["responses"]):
     flash("Trying to access invalid question. Redirecting.")
-    q_id = len(responses)
+    q_id = len(session["responses"])
   # If user has answered all questions redirect to thank you page
-  if len(responses) == 4:
-    return redirect("thanks.html")
+  if len(session["responses"]) == 4:
+    return redirect("/thanks")
 
   return render_template("form.html", question=satisfaction_survey.questions[q_id])
 
 @app.route('/answer', methods=['POST'])
 def answers():
   """Collect answers"""
+  responses = session["responses"]
   responses.append(request.form["choice"])
+  session["responses"] = responses
   if len(responses) < len(satisfaction_survey.questions):
     return redirect("/questions/" + str(len(responses)))
   return redirect("/thanks")
